01a0cfe6129c915c6fcaa77fdbe177a54c5907dd
wooqqq <118504257+wooqqq@users.noreply.github.com>
2023-05-02 13:02:55 +0900

    [level 2] Title: 피보나치 수, Time: 592.24 ms, Memory: 863 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int n) {
+        BigInteger rs = new Fibonacci().calc(n);
+        rs = rs.mod(BigInteger.valueOf(1234567));
+        
+        return rs.intValue();
+    }
+}
+
+class Fibonacci {
+    Map<Integer, BigInteger> cache = new HashMap<>();
+    
+    BigInteger calc(int n) {
+        if (cache.containsKey(n)) return cache.get(n);
+        
+        BigInteger rs;
+        
+        if (n <= 1) {
+            rs = BigInteger.valueOf(n);
+        } else {
+            BigInteger calcOfNMinus2 = calc(n - 2);
+            BigInteger calcOfNMinus1 = calc(n - 1);
+            
+            rs = calcOfNMinus2.add(calcOfNMinus1);
+        }
+        
+        cache.put(n, rs);
+        
+        return rs;
+    }
+}
