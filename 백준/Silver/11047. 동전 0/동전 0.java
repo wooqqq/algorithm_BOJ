43f02b8d2e4abd450834f9f6b626e78c2c4d0113
wooqqq <118504257+wooqqq@users.noreply.github.com>
2023-05-12 09:17:29 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, K;
+    static int[] coin;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        coin = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (coin[i] <= K) {
+                count += (K / coin[i]);
+                K = K % coin[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
