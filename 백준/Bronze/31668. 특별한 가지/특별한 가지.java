f5b2bb14018400183a0e9f05eb8c0f2d69918ba6
wooqqq <118504257+wooqqq@users.noreply.github.com>
2024-04-02 12:02:15 +0900

    [Bronze IV] Title: 특별한 가지, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/31668. 특별한 가지/특별한 가지.java
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -1,11 +1,1 @@
-import java.util.Scanner;
-class Main{
-	public static void main(String[] args){
-		Scanner s=new Scanner(System.in);
-		int[] a=new int[3];
-		int i=0;
-		while (i<3) {
-			a[i++]=s.nextInt();
-		} System.out.print(a[1]/a[0]*a[2]);
-	}
-}
+import java.util.Scanner;class Main{public static void main(String[]args){Scanner s=new Scanner(System.in);int[]a=new int[3];int i=0;while(i<3){a[i++]=s.nextInt();}System.out.print(a[1]/a[0]*a[2]);}}
