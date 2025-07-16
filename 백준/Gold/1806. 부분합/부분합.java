9609cfd9714d15d2b8b214ae96f49a2ef465aa3a
Woo Hyeji <sge0225@naver.com>
2025-07-16 10:14:51 +0900

    [Gold IV] Title: 부분합, Time: 292 ms, Memory: 24120 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, S;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = 0;
+        int len = Integer.MAX_VALUE;
+        int sum = 0;
+
+        while (start <= end && end <= N) {
+            if (sum < S) {
+                sum += arr[end++];
+            } else if (sum >= S) {
+                len = Math.min(len, end - start);
+                sum -= arr[start++];
+            }
+        }
+
+        System.out.println(len == Integer.MAX_VALUE ? 0 : len);
+    }
+}
